e0827812dad4d8e25bd2c0c7c24508c9d7c55459
Zach Pella <127986924+ZachPella@users.noreply.github.com>
2025-05-01 13:42:07 -0500

    Update model_assistant.py

--- model_assistant.py
+++ model_assistant.py
@@ -46,7 +46,7 @@ def local_css():
     
     /* Question label */
     .question-label {
-        font-size: 1rem;
+        font-size: 2rem;
         margin-bottom: 10px;
         color: #333;
     }
@@ -73,7 +73,7 @@ def local_css():
         margin-top: 60px;
         text-align: center;
         color: #555;
-        font-size: 2rem;
+        font-size: 1.5rem;
         display: flex;
         justify-content: center;
         align-items: center;
